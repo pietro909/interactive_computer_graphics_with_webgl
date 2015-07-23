6becbd4ef6951c63c3c2c8fb7c9ad8b6adeb9c38
pietroTP <pietro.grandi.3d@gmail.com>
2015-07-23 21:17:35 +0200

    supporting different line width !

--- assignments/assignment_2.js
+++ assignments/assignment_2.js
@@ -1,10 +1,20 @@
 var
-  gl, LAST_VERTEX = vec2(0, 0), PRESSED = false,
+  gl, CURRENT_SHAPE = 'line', LAST_VERTEX = vec2(0, 0), PRESSED = false, CURRENT_WIDTH = 1,
+  CURRENT_INDEX = 0,
   TOLERANCE = 0.005,
   MAX_POINTS = 1024,
   SIZE_OF_FLOAT = 4,
-  lastArrayVertex = [],
+  currentVertexArray = [],
   vertices = [];
+
+var VertexObject = function (points, width, color) {
+  this.points = [];
+  for (var p = 0; p < points.length; p += 1) {
+    this.points.push(points[p]);
+  }
+  this.width = width;
+  this.color = vec4(1.0, 0.0, 0.0, 1.0);
+};
 
 function printVec2(vec) {
   return vec[0] + ',' + vec[1];
@@ -16,13 +26,13 @@ function printCoords(x, y) {
 
 /**
  * Flatten arrays and count elements
- * @param array {Array} bi-dimensional array
+ * @param array {Array} of VertexObject
  * @returns {number} the number of items
  */
 function countElements(array) {
   var total = 0;
   for (var i = 0; i < array.length; i += 1) {
-    total += array[i].length;
+    total += array[i].points.length;
   }
   return total;
 }
@@ -50,7 +60,12 @@ function getUIElements() {
     infoNormalizedPosition: document.getElementById('normalizedPosition'),
     infoVertices          : document.getElementById('infoVertices'),
     ctrlLineWidth         : document.getElementById('ctrlLineWidth'),
-    ctrlClearCanvas       : document.getElementById('ctrlClearCanvas')
+    ctrlClearCanvas       : document.getElementById('ctrlClearCanvas'),
+    drawType0             : document.getElementById('drawType0'),
+    drawType1             : document.getElementById('drawType1'),
+    drawType2             : document.getElementById('drawType2'),
+    drawType3             : document.getElementById('drawType3'),
+    drawType4             : document.getElementById('drawType3')
   }
 }
 
@@ -81,8 +96,8 @@ window.onload = function () {
    * the buffer for the Color: it is represented as Vec4 and will then take 4 byte * 4 = 16 byte
    */
   var cBufferId = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
-  gl.bufferData(gl.ARRAY_BUFFER, SIZE_OF_FLOAT * 4 * MAX_POINTS, gl.STATIC_DRAW);
+  //gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
+  //gl.bufferData(gl.ARRAY_BUFFER, SIZE_OF_FLOAT * 4 * MAX_POINTS, gl.STATIC_DRAW);
 
   // configure webgl
   gl.viewport(0, 0, uiElements.canvas.width, uiElements.canvas.height);
@@ -98,19 +113,19 @@ window.onload = function () {
   var vPos = gl.getAttribLocation(program, 'vPosition');
   gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vPos);
-
-  var vColor = gl.getAttribLocation(program, 'vColor');
-  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vColor);
-
+  /*
+   var vColor = gl.getAttribLocation(program, 'vColor');
+   gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+   gl.enableVertexAttribArray(vColor);
+   */
   uiElements.ctrlLineWidth.addEventListener('change', function (event) {
-    var width = parseInt(event.target.value, 10);
-    gl.lineWidth(width);
+    CURRENT_WIDTH = parseInt(event.target.value, 10);
   });
 
   uiElements.ctrlClearCanvas.addEventListener('click', function (event) {
     gl.clear(gl.COLOR_BUFFER_BIT);
     vertices = [];
+    uiElements.infoVertices.value = 0 + ' out of ' + MAX_POINTS;
   });
 
   uiElements.canvas.addEventListener('mousedown', function (event) {
@@ -118,7 +133,7 @@ window.onload = function () {
     uiElements.infoLayerPosition.value = printCoords(event.layerX, event.layerY);
     uiElements.infoIsMouseDown.value = 'DOWN';
     PRESSED = true;
-    vertices.push(lastArrayVertex);
+    CURRENT_INDEX += 1;
     uiElements.infoNormalizedPosition.value = printCoords(glCoords.x, glCoords.y);
   });
 
@@ -127,7 +142,7 @@ window.onload = function () {
     uiElements.infoNormalizedPosition.value = printCoords(glCoords.x, glCoords.y);
     uiElements.infoLayerPosition.value = printCoords(event.layerX, event.layerY);
     uiElements.infoIsMouseDown.value = '';
-    lastArrayVertex = [];
+    currentVertexArray = [];
     PRESSED = false;
   });
 
@@ -137,14 +152,16 @@ window.onload = function () {
     uiElements.infoLayerPosition.value = printCoords(event.layerX, event.layerY);
     uiElements.infoNormalizedPosition.value = printCoords(glCoords.x, glCoords.y);
     if (PRESSED) {
-      //console.log(LAST_VERTEX[0], glCoords.x);
       if (Math.abs(LAST_VERTEX[0] - glCoords.x) > TOLERANCE) {
         var total = countElements(vertices);
         if (total === MAX_POINTS) {
           vertices = [];
         }
-        console.log('current point: '+printVec2(currentPoint));
-        lastArrayVertex.push(currentPoint);
+        //console.log('current point: ' + printVec2(currentPoint));
+        currentVertexArray.push(currentPoint);
+        vertices.push(new VertexObject(
+          currentVertexArray, CURRENT_WIDTH, ''
+        ));
         uiElements.infoVertices.value = total + ' out of ' + MAX_POINTS;
         LAST_VERTEX = currentPoint;
         render();
@@ -152,22 +169,60 @@ window.onload = function () {
     }
   });
 
+  function onDrawChange(event) {
+    if (event.target.checked) {
+      CURRENT_SHAPE = event.target.value;
+    }
+  }
+
+  uiElements.drawType0.addEventListener('change', onDrawChange);
+  uiElements.drawType1.addEventListener('change', onDrawChange);
+  uiElements.drawType2.addEventListener('change', onDrawChange);
+  uiElements.drawType3.addEventListener('change', onDrawChange);
+  uiElements.drawType4.addEventListener('change', onDrawChange);
 
   function render() {
-    var currentPoints, color = vec4(1.0, 0.0, 0.0, 1.0);
+    var currentPoints, color = vec4(Math.random(), Math.random(), 0.0, 1.0), start = 0;
+    console.log(CURRENT_SHAPE);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 16 * countElements(vertices), flatten(color));
+    //gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
+    //gl.bufferSubData(gl.ARRAY_BUFFER, SIZE_OF_FLOAT * 4 * countElements(vertices), flatten(color));
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    //gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    //gl.bufferSubData(gl.ARRAY_BUFFER, SIZE_OF_FLOAT * 2 * countElements(vertices), flatten(vertices));
 
     for (var i = 0; i < vertices.length; i += 1) {
       currentPoints = vertices[i];
-      gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(currentPoints));
+
+      //gl.lineWidth(CURRENT_WIDTH);
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+      gl.bufferSubData(gl.ARRAY_BUFFER, start, flatten(currentPoints.points));
+
+      gl.lineWidth(currentPoints.width);
+      /*
+       gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
+       gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(color));
+       */
       if (currentPoints.length == 1) {
-        gl.drawArrays(gl.POINTS, 0, currentPoints.length);
+        gl.drawArrays(gl.POINTS, start, currentPoints.points.length);
       } else {
-        gl.drawArrays(gl.LINE_STRIP, 0, currentPoints.length);
+        switch (CURRENT_SHAPE) {
+          case 'lines' :
+            gl.drawArrays(gl.LINES, start, currentPoints.points.length);
+            break;
+          case 'line' :
+            gl.drawArrays(gl.LINE_STRIP, start, currentPoints.points.length);
+            break;
+          case 'triangle_fan':
+            gl.drawArrays(gl.TRIANGLE_FAN, start, currentPoints.points.length);
+            break;
+          case 'triangle_loop':
+            gl.drawArrays(gl.TRIANGLE_LOOP, start, currentPoints.points.length);
+            break;
+          default :
+            gl.drawArrays(gl.POINTS, start, currentPoints.points.length);
+        }
       }
     }
 
